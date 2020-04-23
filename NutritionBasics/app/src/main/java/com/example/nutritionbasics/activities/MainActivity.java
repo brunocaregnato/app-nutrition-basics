6d3faa366062296466442e2c39f73a3b54282afd
brunocaregnato <bmcaregnato@ucs.br>
2020-04-22 22:16:33 -0300

    add fragments

--- NutritionBasics/app/src/main/java/com/example/nutritionbasics/activities/MainActivity.java
+++ NutritionBasics/app/src/main/java/com/example/nutritionbasics/activities/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.nutritionbasics;
+package com.example.nutritionbasics.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
@@ -11,6 +12,13 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.nutritionbasics.R;
+import com.example.nutritionbasics.activities.fragments.Details;
+import com.example.nutritionbasics.activities.fragments.Evolution;
+import com.example.nutritionbasics.activities.fragments.History;
+import com.example.nutritionbasics.activities.fragments.Home;
+import com.example.nutritionbasics.activities.fragments.Profile;
+import com.example.nutritionbasics.activities.fragments.RegisterMeal;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     Toolbar toolbar;
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
+    private Home home;
 
     @SuppressLint("RestrictedApi")
     @Override
@@ -35,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -42,25 +52,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         switch (menuItem.getItemId()){
             case R.id.home:
                 Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Home()).commit();
                 break;
             case R.id.profile:
                 Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Profile()).commit();
                 break;
             case R.id.registerMeal:
                 Toast.makeText(MainActivity.this, "Register Meal", Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new RegisterMeal()).commit();
                 break;
             case R.id.details:
                 Toast.makeText(MainActivity.this, "Details", Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Details()).commit();
                 break;
             case R.id.history:
                 Toast.makeText(MainActivity.this, "History", Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new History()).commit();
                 break;
             case R.id.evolution:
                 Toast.makeText(MainActivity.this, "Evolution", Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Evolution()).commit();
                 break;
-
-
         }
-        return false;
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
